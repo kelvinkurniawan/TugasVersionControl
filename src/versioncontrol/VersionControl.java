9fad04aa5fb6420ecc508ed7d6737247a3374879
Kelvin Kurniawan O <tepinnko@gmail.com>
2020-10-02 00:02:47 +0700

    Add homescreen and add transaction screen

--- src/versioncontrol/VersionControl.java
+++ src/versioncontrol/VersionControl.java
@@ -5,17 +5,63 @@
  */
 package versioncontrol;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author kelvi
  */
 public class VersionControl {
+    
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/tgsversioncontrol";
+    static final String USER = "root";
+    static final String PASS = "";
 
+    
+    static Connection conn;
+    static Statement stmt;
+    static ResultSet rs;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   public static void main(String[] args) {
+        
+        // Melakukan koneksi ke database
+        // harus dibungkus dalam blok try/catch
+        try {
+            // register driver yang akan dipakai
+            Class.forName(JDBC_DRIVER);
+            
+            // buat koneksi ke database
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            
+            // buat objek statement
+            stmt = conn.createStatement();
+            
+            // buat query ke database
+            String sql = "SELECT * FROM users";
+            
+            // eksekusi query dan simpan hasilnya di obj ResultSet
+            rs = stmt.executeQuery(sql);
+            
+            // tampilkan hasil query
+            while(rs.next()){
+                System.out.println("User ID: " + rs.getInt("UserId"));
+                System.out.println("Nama: " + rs.getString("name"));
+                System.out.println("Email: " + rs.getString("email"));
+            }
+            
+            stmt.close();
+            conn.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
     
 }
